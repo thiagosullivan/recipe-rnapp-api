67731e872041e7780a0d0720459fcc276dde3808
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-19 23:53:58 -0300

    feat: favorite recipe endpoint

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
-import { dizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { ENV } from "./env.js";
 import * as schema from "../db/schema.js";
 
 const sql = neon(ENV.DATABASE_URL);
-export const db = dizzle(sql, { schema });
+export const db = drizzle(sql, { schema });
