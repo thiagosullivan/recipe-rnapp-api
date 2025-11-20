8a2004325d1ff6f2ff6469a85843c1dbd3ebbba4
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-19 23:54:28 -0300

    feat: delete favorite recipe endpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -33,6 +34,26 @@ app.post("/api/favorites", async (req, res) => {
     res.status(201).json(newFavorite[0]);
   } catch (error) {
     console.log("Error adding favorite");
+    res.status(500).json({ error: "Something went wrong!" });
+  }
+});
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(
+        and(
+          eq(favoritesTable.userId, userId),
+          eq(favoritesTable.recipeId, parseInt(recipeId))
+        )
+      );
+
+    res.status(200).json({ message: "Favorite removed successfully" });
+  } catch (error) {
+    console.log("Error removing favorite");
     res.status(500).json({ error: "Something went wrong!" });
   }
 });
